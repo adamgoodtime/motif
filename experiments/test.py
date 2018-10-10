20564673b2c3df84d7adc6cd915f9cba58b25d08
such-a-git <adamperrett3142@gmail.com>
2018-10-10 18:06:35 +0100

    Added changes from other branch, about to change loads of shit

--- experiments/test.py
+++ experiments/test.py
@@ -3,11 +3,34 @@ from methods.networks import motif_population
 print "starting"
 
 #check max motif count
-motif_pop = motif_population(max_motif_size=4,
-                             no_weight_bins=7,
-                             no_delay_bins=7,
-                             population_size=200)
+motif_pop = motif_population(max_motif_size=3,
+                             no_weight_bins=6,
+                             no_delay_bins=6,
+                             population_size=500)
 
-agent_pop = motif_pop.generate_agents()
+motif_pop.generate_agents(inputs=6, outputs=2)
+
+# convert motifs to networks
+agent_pop = motif_pop.convert_population()
+
+# evaluate
+
+# adjust population weights and clean up unused motifs
+
+# generate offspring
+    # mutate the individual and translate it to a new motif
+    # connection change
+    # swap motif
 
 print "done"
+
+#ToDo
+'''
+figure out mapping to inputs
+    have a fixed network but synaptic plasticity on IO
+    have a IO metric attached to each motif
+account for rotations but the same motif
+    compare with indexes swapped around
+should a motif of motifs become a motif in it's own right
+    store original motif and child motifs in agent if successful create new motif, label level it was useful?
+'''
--- methods/networks.py
+++ methods/networks.py
@@ -73,6 +73,8 @@ class motif_population(object):
         self.motif_configs = {}  # Tuple of tuples(node types, node i/o P(), connections, selection weight)
         self.motifs_generated = 0
         self.total_weight = 0
+        self.agent_pop = []
+        self.agent_nets = {}
 
         true_or_false = [True, False]
 
@@ -80,27 +82,28 @@ class motif_population(object):
             print "generating population"
             if self.population_seed is not None:
                 for seed in self.population_seed:
+                    # or just make this an insert and start = motifs_gened
                     self.motif_configs.update(seed)
                 start_point = len(self.population_seed)
             else:
                 start_point = 0
-            if self.discrete_params and not self.multi_synapse:
-                maximum_number_of_motifs = 1
-                for i in range(self.min_motif_size, self.max_motif_size + 1):
-                    maximum_number_of_motifs *= math.pow(4, i)  # possible connections
-                if self.weights:
-                    maximum_number_of_motifs *= self.no_weight_bins
-                if self.delays:
-                    maximum_number_of_motifs *= self.no_delay_bins
-                if self.io_config == 'fixed':
-                    maximum_number_of_motifs *= math.pow(4, i)  # possible io configs
-                maximum_number_of_motifs *= math.pow(2, i)     # exit/inhib
-            if self.population_size > maximum_number_of_motifs:
-                print "\nPopulation size is bigger than the full spectrum of possible motifs.\n" \
-                      "Repeats will be allowed during generation.\n"
-                repeats = True
-            else:
-                repeats = False
+            # if self.discrete_params and not self.multi_synapse:
+            #     maximum_number_of_motifs = 1
+            #     for i in range(self.min_motif_size, self.max_motif_size + 1):
+            #         maximum_number_of_motifs *= math.pow(4, i)  # possible connections
+            #     if self.weights:
+            #         maximum_number_of_motifs *= self.no_weight_bins
+            #     if self.delays:
+            #         maximum_number_of_motifs *= self.no_delay_bins
+            #     if self.io_config == 'fixed':
+            #         maximum_number_of_motifs *= math.pow(4, i)  # possible io configs
+            #     maximum_number_of_motifs *= math.pow(2, i)     # exit/inhib
+            # if self.population_size > maximum_number_of_motifs:
+            #     print "\nPopulation size is bigger than the full spectrum of possible motifs.\n" \
+            #           "Repeats will be allowed during generation.\n"
+            #     repeats = True
+            # else:
+            #     repeats = False
             i = start_point
             while i < population_size:
                 motif = {}
@@ -132,11 +135,11 @@ class motif_population(object):
                 motif['depth'] = 1
                 # motif['id'] = self.motifs_generated
                 if self.selection_metric == 'fitness':
-                    motif['weight'] = 1
+                    weight = 1
                 # if not repeats:
                 if not self.id_check(motif):
                     # print self.id_check(motif)
-                    self.insert_motif(motif)
+                    self.insert_motif(motif, weight)
                 else:
                     print "repeated ", i, self.id_check(motif)
                     i -= 1
@@ -151,7 +154,10 @@ class motif_population(object):
     def id_check(self, motif):
         motif_id = False
         for config in self.motif_configs:
-            if self.motif_configs[config] == motif:
+            #compare each element and return rotations with which they are similar
+            if self.motif_configs[config]['node'] == motif['node'] and \
+                    self.motif_configs[config]['io'] == motif['io'] and \
+                    self.motif_configs[config]['conn'] == motif['conn']:
                 motif_id = config
                 break
         return motif_id
@@ -168,68 +174,100 @@ class motif_population(object):
         # return self.motif_configs[motif]
         return motif
 
-    def insert_motif(self, motif):
+    def insert_motif(self, motif, weight=0):
         check = self.id_check(motif)
         if not check:
-            self.motif_configs['{}'.format(self.motifs_generated)] = motif
+            motif_id = self.motifs_generated
+            self.motif_configs['{}'.format(motif_id)] = motif
+            self.motif_configs['{}'.format(motif_id)]['weight'] = weight
+            self.motif_configs['{}'.format(motif_id)]['id'] = motif_id
             self.motifs_generated += 1
-            return '{}'.format(self.motifs_generated-1)
+            # print motif_id
+            return '{}'.format(self.motifs_generated - 1)
         else:
             return check
 
-    def motif_of_motif(self, motif_id, config):
+    def motif_of_motif(self, motif_id, config, max_depth, current_depth=0):
         # add layer at lowest level
         # add layer at specific level
         # add motif with a certain probability
         # possibly combine with a mutate operation?
         motif = deepcopy(self.motif_configs[motif_id])
-        i = 0
-        layer = 0
-        if config == 'lowest':
-            None
-        elif config <= 1:
-            for node in motif['node']:
-                if node == 'excitatory' or node == 'inhibitory':
-                    if np.random.random() < config:
-                        # sub_motif = self.select_motif()
-                        # motif['node'][i] = self.id_check(sub_motif)
-                        motif['node'][i] = self.select_motif()
-                        motif['depth'] += 1 #depth calculation is wrong
-                else:
-                    sub_motif = self.motif_of_motif(node, config)
-                    # sub_motif_id = self.insert_motif(sub_motif)
-                    # motif['node'][i] = sub_motif[sub_motif_id]
-                    motif['node'][i] = self.insert_motif(sub_motif)
-                    motif['depth'] += 1 #again depth is probs wrong
-                i += 1
-        else:
-            # go to a certain depth
-            None
+        if current_depth < max_depth:
+            i = 0
+            layer = 0
+            if config == 'lowest':
+                None
+            elif config <= 1:
+                for node in motif['node']:
+                    if node == 'excitatory' or node == 'inhibitory':
+                        if np.random.random() < config:
+                            # sub_motif = self.select_motif()
+                            # motif['node'][i] = self.id_check(sub_motif)
+                            motif['node'][i] = self.select_motif()
+                            motif['depth'] += 1  # depth calculation is wrong
+                    else:
+                        sub_motif = self.motif_of_motif(node, config, max_depth, current_depth + 1)
+                        # sub_motif_id = self.insert_motif(sub_motif)
+                        # motif['node'][i] = sub_motif[sub_motif_id]
+                        motif['node'][i] = self.insert_motif(sub_motif)
+                        motif['depth'] += 1  # again depth is probs wrong
+                    i += 1
+            else:
+                # go to a certain depth
+                None
         return motif
 
-    def generate_agents(self,
-                        pop_size=200,
-                        start_small=False):
+    def generate_agents(self, inputs, outputs, pop_size=200, start_small=False, max_depth=2):
         print "constructing population of agents"
+        self.agent_pop = []
         for i in range(pop_size):
             # select depth of the agent
-            if not start_small:
-                depth = np.random.randint(self.initial_hierarchy_depth, self.max_hierarchy_depth)
+            if not start_small:  # this is broke af
+                depth = np.random.randint(self.initial_hierarchy_depth, max_depth + 1)
             else:
                 depth = self.initial_hierarchy_depth
-            motif = None
+            # motif = None
+            motif = self.select_motif()
             # generate the agent
-            for j in range(depth):
-                # check if it's the first iteration
-                if motif is None:
-                    motif = self.select_motif()
-                else:
-                    motif = self.motif_of_motif(motif, 1)
+            # for j in range(depth):
+            # check if it's the first iteration
+            if motif is None:
+                motif = self.select_motif()
+            else:
+                for i in range(depth):
+                    motif = self.motif_of_motif(motif, 1, depth, i)
                     motif = self.insert_motif(motif)
-            agent = motif
+            self.agent_pop.append(motif)
+        return self.agent_pop
+
+    def read_motif(self, motif_id, e2e=[], e2i=[], excit_count=0, i2i=[], i2e=[], inhib_count=0, top_layer=True):
+        motif = self.motif_configs[motif_id]
+        for node in motif['node']:
+            if node == 'excitatory':
+                excit_count += 1
+            elif node == 'inhibitory':
+                inhib_count += 1
+            else:
+                self.read_motif(node, e2e, e2i, excit_count, i2i, i2e, inhib_count, False)
+        if top_layer:
+            return e2e, e2i, i2i, i2e
+        else:
+            return e2e, e2i, excit_count, i2i, i2e, inhib_count
+
+    def convert_population(self):
+        for agent in self.agent_pop:
+            [e2e, e2i, i2i, i2e] = self.read_motif(agent)
 
 
-        return 0
-
-
-
+class species(object):
+    def __init__(self, initial_member):
+        self.members = [initial_member]
+        self.representative = initial_member
+        self.offspring = 0
+        self.age = 0
+        self.avg_fitness = 0.
+        self.max_fitness = 0.
+        self.max_fitness_prev = 0.
+        self.no_improvement_age = 0
+        self.has_best = False
