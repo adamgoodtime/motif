9ecfccf205b3b0e85787ac11741072f35d74d749
such-a-git <adamperrett3142@gmail.com>
2018-10-10 17:05:37 +0100

    Attempting to construct the network using coordinate based motifs to allowing easy mapping of IO

--- experiments/test.py
+++ experiments/test.py
@@ -3,11 +3,23 @@ from methods.networks import motif_population
 print "starting"
 
 #check max motif count
-motif_pop = motif_population(max_motif_size=4,
-                             no_weight_bins=7,
-                             no_delay_bins=7,
-                             population_size=200)
+motif_pop = motif_population(max_motif_size=3,
+                             no_weight_bins=6,
+                             no_delay_bins=6,
+                             population_size=500)
 
-agent_pop = motif_pop.generate_agents()
+motif_pop.generate_agents(inputs=6, outputs=2)
+
+# convert motifs to networks
+agent_pop = motif_pop.convert_population()
+
+# evaluate
+
+# adjust population weights and clean up unused motifs
+
+# generate offspring
+    # mutate the individual and translate it to a new motif
+    # connection change
+    # swap motif
 
 print "done"
