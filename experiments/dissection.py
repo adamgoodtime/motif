89ad6f57513a5631f6c8a161f40b73919bcd513a
such-a-git <adamperrett3142@gmail.com>
2018-11-24 18:17:14 +0000

    Added the beginnings of a new file

---
+++ experiments/dissection.py
@@ -0,0 +1,3 @@
+#todo here will be where an agent and a motif population will be read in to examine what is present in a well performing agent
+
+#todo could also do motif analysis and progression throught the simulation
