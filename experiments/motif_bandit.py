0f73f1796b8a0ca7354010d9064d6f3af37a2486
Owner <adamperrett3142@gmail.com>
2018-12-09 20:52:20 +0000

    Slight experimental change, fixed number of arms bug

--- experiments/motif_bandit.py
+++ experiments/motif_bandit.py
@@ -8,6 +8,8 @@ from multiprocessing.pool import ThreadPool
 import multiprocessing
 import pathos.multiprocessing
 
+connections = []
+
 def bandit(generations):
     print "starting"
 
@@ -16,7 +18,7 @@ def bandit(generations):
     agent_pop_size = 100
     arm1 = 0.9
     arm2 = 0.1
-    arm3 = 0.3
+    arm3 = 0.1
     arm_len = 1
     arms = []
     for i in range(arm_len):
@@ -31,21 +33,22 @@ def bandit(generations):
             [0.2, 0.1, 0.3, 0.1, 0.2, 0.1, 0.9, 0.2], [0.1, 0.3, 0.2, 0.2, 0.1, 0.2, 0.1, 0.9]]
     # '''
     if isinstance(arms[0], list):
-        number_of_arms = len(arms)
+        number_of_arms = len(arms[0])
     else:
         number_of_arms = len(arms)
 
     split = 1
 
     reward_shape = False
-    reward = 1
+    reward = 0
     noise_rate = 0
     noise_weight = 0.01
-    maximum_depth = 5
+    maximum_depth = 10
     size_fitness = False
     spikes_fitness = False
     random_arms = 0
     viable_parents = 0.3
+    elitism = 0.3
 
     # check max motif count
     motifs = motif_population(max_motif_size=3,
@@ -54,19 +57,40 @@ def bandit(generations):
                               weight_range=(0.005, weight_max),
                               # delay_range=(2, 2.00001),
                               neuron_types=(['excitatory', 'inhibitory']),
-                              io_weight=[2, number_of_arms, 0.6],
+                              io_weight=[2, number_of_arms, 1],
                               # read_entire_population='motif population 0: conf.csv',
                               population_size=200)
 
     # todo :add number of different motifs to the fitness function to promote regularity
-
-    config = "bandit reward_shape:{}, reward:{}, noise r-w:{}-{}, arms:{}-{}-{}, max_d{}, size:{}, spikes:{}, w_max{}, rents{}".format(
-        reward_shape, reward, noise_rate, noise_weight, arms[0], len(arms), random_arms, maximum_depth, size_fitness, spikes_fitness, weight_max, viable_parents)
     # config = "bandit reward_shape:{}, reward:{}, noise r-w:{}-{}, arms:{}-{}-{}, max_d{}, size:{}, spikes:{}, w_max{}".format(
     #     reward_shape, reward, noise_rate, noise_weight, arms[0], len(arms), random_arms, maximum_depth, size_fitness, spikes_fitness, weight_max)
 
-    agents = agent_population(motifs, pop_size=agent_pop_size, inputs=2, outputs=number_of_arms,
-                              maximum_depth=maximum_depth, viable_parents=viable_parents)
+    agents = agent_population(motifs,
+                              pop_size=agent_pop_size,
+                              inputs=2,
+                              outputs=number_of_arms,
+                              elitism=elitism,
+                              maximum_depth=maximum_depth,
+                              viable_parents=viable_parents)
+
+    config = "bandit reward_shape:{}, reward:{}, noise r-w:{}-{}, arms:{}-{}-{}, max_d:{}, size:{}, spikes:{}, " \
+             "w_max:{}, rents:{}, elitism:{}, pop_size:{}".format(reward_shape, reward, noise_rate, noise_weight,
+                                                                  arms[0], len(arms), random_arms, maximum_depth,
+                                                                  size_fitness, spikes_fitness,weight_max,
+                                                                  viable_parents, elitism, agent_pop_size)
+
+    globals()['pop_size'] = agent_pop_size
+    globals()['config'] = config
+    # globals()['connections'] = connections
+    globals()['arms'] = arms
+    globals()['split'] = split
+    globals()['runtime'] = 41000
+    globals()['reward'] = reward
+    globals()['noise_rate'] = noise_rate
+    globals()['noise_weight'] = noise_weight
+    globals()['size_f'] = size_fitness
+    globals()['spike_f'] = spikes_fitness
+    globals()['exposure_time'] = 200
 
     for i in range(generations):
 
@@ -88,18 +112,6 @@ def bandit(generations):
 
         # fitnesses = agents.thread_bandit(connections, arms, split=16, runtime=21000, exposure_time=200, reward=reward, noise_rate=noise_rate, noise_weight=noise_weight, size_f=size_fitness, spike_f=spikes_fitness)
 
-        globals()['pop_size'] = agent_pop_size
-        globals()['config'] = config
-        globals()['connections'] = connections
-        globals()['arms'] = arms
-        globals()['split'] = split
-        globals()['runtime'] = 41000
-        globals()['reward'] = reward
-        globals()['noise_rate'] = noise_rate
-        globals()['noise_weight'] = noise_weight
-        globals()['size_f'] = size_fitness
-        globals()['spike_f'] = spikes_fitness
-        globals()['exposure_time'] = 200
         # config = 'test'
         if config != 'test':
             # arms = [0.1, 0.9, 0.2]
