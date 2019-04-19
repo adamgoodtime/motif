4268d6787204fe9b30d40567fe4863a053301b13
such-a-git <adam.perrett@manchester.ac.uk>
2019-04-19 18:38:44 +0100

    slight changes to functionality of returned fail catching

--- methods/exec_subprocess.py
+++ methods/exec_subprocess.py
@@ -157,12 +157,13 @@ def subprocess_experiments(connections, test_data_set, split=4, runtime=2000, ex
 
     for i in range(len(pool_result)):
         try:
-            result = pool_result[i][0]
+            test_pool = pool_result[i][0]
+            test_pool = pool_result[i][1]
         except:
+            traceback.print_exc()
             print "it broke and the except caught the return failure"
-            pool_result = ['fail', 'fail']
+            pool_result[i] = ['fail', 'fail']
         if pool_result[i][0] == 'fail' and len(connection_threads[i][0]) > 1:
-            traceback = pool_result[i][1]
             pool_result[i] = pool_result[i][1]
             if plasticity == 'pall':
                 # new_fail = False
@@ -188,7 +189,6 @@ def subprocess_experiments(connections, test_data_set, split=4, runtime=2000, ex
             pool_result[i] = subprocess_experiments(connection_threads[i][0], problem_arms, split, runtime,
                                                 exposure_time, noise_rate, noise_weight, spike_f, top=False, parallel=parallel, make_action=make_action)
         elif pool_result[i][0] == 'fail' and len(connection_threads[i][0]) == 1:
-            traceback = pool_result[i][1]
             pool_result[i] = pool_result[i][1]
             new_fail = False
             connection_threads[i].append(pool_result[i])
